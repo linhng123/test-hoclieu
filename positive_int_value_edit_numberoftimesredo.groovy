55b3366a38baf11bb7eabe36876197a9f75f9799
linhng123 <47728299+linhng123@users.noreply.github.com>
2019-04-10 13:30:40 +0700

    Add files via upload

---
+++ positive_int_value_edit_numberoftimesredo.groovy
@@ -0,0 +1,37 @@
+import static com.kms.katalon.core.checkpoint.CheckpointFactory.findCheckpoint
+import static com.kms.katalon.core.testcase.TestCaseFactory.findTestCase
+import static com.kms.katalon.core.testdata.TestDataFactory.findTestData
+import static com.kms.katalon.core.testobject.ObjectRepository.findTestObject
+import com.kms.katalon.core.checkpoint.Checkpoint as Checkpoint
+import com.kms.katalon.core.cucumber.keyword.CucumberBuiltinKeywords as CucumberKW
+import com.kms.katalon.core.mobile.keyword.MobileBuiltInKeywords as Mobile
+import com.kms.katalon.core.model.FailureHandling as FailureHandling
+import com.kms.katalon.core.testcase.TestCase as TestCase
+import com.kms.katalon.core.testdata.TestData as TestData
+import com.kms.katalon.core.testobject.TestObject as TestObject
+import com.kms.katalon.core.webservice.keyword.WSBuiltInKeywords as WS
+import com.kms.katalon.core.webui.keyword.WebUiBuiltInKeywords as WebUI
+import internal.GlobalVariable as GlobalVariable
+
+WebUI.openBrowser('')
+
+WebUI.navigateToUrl('https://hoclieu.sachmem.vn/')
+
+WebUI.click(findTestObject('Page_Hc liu - Sch Mm/a_ng nhp'))
+
+WebUI.setText(findTestObject('Page_ng nhp  Sch Mm/input_a ch email_useremail'), 'linhnguyen.uet1210@gmail.com')
+
+WebUI.setEncryptedText(findTestObject('Page_ng nhp  Sch Mm/input_Mt khu_userpassword'), 'RigbBhfdqODKcAsiUrg+1Q==')
+
+WebUI.click(findTestObject('Page_Hc liu - Sch Mm/a_Lp hc ca ti'))
+
+WebUI.click(findTestObject('Page_Hc liu - Sch Mm/a_Abc'))
+
+WebUI.click(findTestObject('Page_Hc liu - Sch Mm/a_S hc sinh vo lm bi 0_optionDropdown'))
+
+WebUI.click(findTestObject('Page_Hc liu - Sch Mm/a_Chnh sa'))
+
+WebUI.setText(findTestObject('Page_Hc liu - Sch Mm/input_S ln lm li_form-control ng-untouched ng-valid ng-dirty'), '2')
+
+WebUI.click(findTestObject('Page_Hc liu - Sch Mm/input__btn btn-primary'))
+
